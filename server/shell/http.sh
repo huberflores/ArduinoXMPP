894041238ac50049e6f701122b4de82638765ce8
lauris <lauris.kruusamae@mobisolutions.com>
2013-04-26 17:01:51 +0300

    updated http server startup script

--- server/shell/http.sh
+++ server/shell/http.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -cp ../target/XMPPProject-0.0.1-SNAPSHOT-full.jar ee.ut.ati.masters.Main
+nohup java -cp ../target/XMPPProject-0.0.1-SNAPSHOT-full.jar ee.ut.ati.masters.Main </dev/null 2>&1 >> logfile.log &
